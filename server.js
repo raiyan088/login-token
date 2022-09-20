3556b57d471be9287914af1bda51cf714e98a5d1
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-09-21 04:59:54 +0600

    Update server.js

--- server.js
+++ server.js
@@ -18,77 +18,11 @@ let options = {}
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     chrome = require("chrome-aws-lambda")
     puppeteer = require("puppeteer-core")
-    
-    options ={
-      args: [
-        '--autoplay-policy=user-gesture-required',
-        '--disable-background-networking',
-        '--disable-background-timer-throttling',
-        '--disable-backgrounding-occluded-windows',
-        '--disable-breakpad',
-        '--disable-client-side-phishing-detection',
-        '--disable-component-update',
-        '--disable-default-apps',
-        '--disable-dev-shm-usage',
-        '--disable-domain-reliability',
-        '--disable-extensions',
-        '--disable-features=AudioServiceOutOfProcess',
-        '--disable-hang-monitor',
-        '--disable-ipc-flooding-protection',
-        '--disable-notifications',
-        '--disable-offer-store-unmasked-wallet-cards',
-        '--disable-popup-blocking',
-        '--disable-print-preview',
-        '--disable-prompt-on-repost',
-        '--disable-renderer-backgrounding',
-        '--disable-setuid-sandbox',
-        '--disable-speech-api',
-        '--disable-sync',
-        '--disk-cache-size=33554432',
-        '--hide-scrollbars',
-        '--ignore-gpu-blocklist',
-        '--metrics-recording-only',
-        '--mute-audio',
-        '--no-default-browser-check',
-        '--no-first-run',
-        '--no-pings',
-        '--no-sandbox',
-        '--no-zygote',
-        '--password-store=basic',
-        '--use-gl=swiftshader',
-        '--use-mock-keychain',
-        '--single-process',
-        '--hide-scrollbars',
-        '--disable-web-security'
-      ],
-      defaultViewport: {
-        deviceScaleFactor: 1,
-        hasTouch: false,
-        height: 1080,
-        isLandscape: true,
-        isMobile: false,
-        width: 1920
-      },
-      executablePath: '/tmp/chromium',
-      headless: true,
-      ignoreHTTPSErrors: true
-    }
 } else {
     puppeteer = require("puppeteer")
-    
-    options = {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
 }
 
-console.log('Start: '+new Date().getTime())
-
-;(async () => {
-    if(browser == null) browser = await puppeteer.launch(options)
-
-    if(page == null) page = (await browser.pages())[0]
-})() 
+console.log('Start: '+new Date().getTime()) 
 
 app.get('/page', async function(req, res) {
     if(page == null) {
@@ -102,16 +36,9 @@ app.get('/page', async function(req, res) {
     }
 })
 
-app.get('/data', async function(req, res) {
-    res.writeHeader(200, {"Content-Type": "text/html"})
-    res.write(JSON.stringify(options))
-    res.end()
-})
-
-app.get("/check", async (req, res) => {
-  options = {}
-
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+app.get('/', async function(req, res) {
+    if(browser == null) {
+        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
             options = {
               args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
               defaultViewport: chrome.defaultViewport,
@@ -125,7 +52,17 @@ app.get("/check", async (req, res) => {
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
             }
         }
+        browser = await puppeteer.launch(options)
+    }
+    
+    if(page == null) page = (await browser.pages())[0]
+    
+    res.writeHeader(200, {"Content-Type": "text/html"})
+    res.write('Start')
+    res.end()
+})
 
+app.get("/check", async (req, res) => {
   try {
     await page.goto('https://google.com')
     res.send(await page.title())
