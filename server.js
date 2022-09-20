be8f0e000fd9eea290d7d461c7a7300f9e140044
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-09-21 04:08:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const app = require("express")();
+const app = require("express")()
 
-let chrome = {};
-let puppeteer;
+let chrome = {}
+let puppeteer
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
+  chrome = require("chrome-aws-lambda")
+  puppeteer = require("puppeteer-core")
 } else {
-  puppeteer = require("puppeteer");
+  puppeteer = require("puppeteer")
 }
 
-app.get("/api", async (req, res) => {
-  let options = {};
+app.get("/check", async (req, res) => {
+  let options = {}
 
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     options = {
@@ -20,23 +20,23 @@ app.get("/api", async (req, res) => {
       executablePath: await chrome.executablePath,
       headless: true,
       ignoreHTTPSErrors: true,
-    };
+    }
   }
 
   try {
-    let browser = await puppeteer.launch(options);
+    let browser = await puppeteer.launch(options)
 
-    let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+    let page = await browser.newPage()
+    await page.goto("https://www.google.com")
+    res.send(await page.title())
   } catch (err) {
-    console.error(err);
+    console.error(err)
     return null;
   }
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+  console.log("Server started")
 });
 
-module.exports = app;
+module.exports = app
