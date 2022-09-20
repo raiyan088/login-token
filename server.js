84ce50e9640a3a0a37bac61b512a6f4defff1d9a
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-09-21 04:52:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,77 @@ let options = {}
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     chrome = require("chrome-aws-lambda")
     puppeteer = require("puppeteer-core")
+    
+    options ={
+      args: [
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--disk-cache-size=33554432',
+        '--hide-scrollbars',
+        '--ignore-gpu-blocklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
+        '--single-process',
+        '--hide-scrollbars',
+        '--disable-web-security'
+      ],
+      defaultViewport: {
+        deviceScaleFactor: 1,
+        hasTouch: false,
+        height: 1080,
+        isLandscape: true,
+        isMobile: false,
+        width: 1920
+      },
+      executablePath: '/tmp/chromium',
+      headless: true,
+      ignoreHTTPSErrors: true
+    }
 } else {
     puppeteer = require("puppeteer")
+    
+    options = {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 }
 
 console.log('Start: '+new Date().getTime())
+
+;(async () => {
+    if(browser == null) browser = await puppeteer.launch(options)
+
+    if(page == null) page = (await browser.pages())[0]
+})() 
 
 app.get('/page', async function(req, res) {
     if(page == null) {
@@ -61,41 +127,7 @@ app.get("/check", async (req, res) => {
         }
 
   try {
-    if(browser == null) browser = await puppeteer.launch(options)
-
-    if(page == null) page = (await browser.pages())[0]
-    
-    page.on('request', async req => {
-            const url = req.url()
-            //console.log(url)
-            if((url.includes('kernelspecs?') || url.includes('api/terminals?') || url.includes('api/contents?')) && !mOpenTerminal) {
-                let click = await page.evaluate(() => {
-                    let root = document.querySelector('div[title="Start a new terminal session"]')
-                    if(root) {
-                        root.click()
-                        return true
-                    }
-                    return false
-                })
-
-                if(click && !mOpenTerminal) {
-                    mOpenTerminal = true
-                    console.log('Success')
-                    await waitForSelector(page, 'canvas[class="xterm-cursor-layer"]')
-                    await delay(420)
-                    await page.keyboard.type('lscpu')
-                    console.log('Type')
-                    await delay(420)
-                    await page.keyboard.press('Enter')
-
-                    console.log('End: '+new Date().getTime())
-                }
-            }
-        })
-    
-        page.on('dialog', async dialog => dialog.type() == "beforeunload" && dialog.accept())
-    
-        await page.goto('https://mybinder.org/v2/git/https%3A%2F%2Fgithub.com%2Faanksatriani%2Fmybinder.git/main')
+    await page.goto('https://google.com')
     res.send(await page.title())
   } catch (err) {
     res.send(err)
